ce29acdc8f6c4417626d481239dd4f9b9350ab7a
Iurii Karpov <y.karpov@dats.team>
2023-12-19 17:33:03 +0100

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+7
+App.tsx,2/f/2f3db35b9bba122792d724b9b48a0aac92ebbb79
+>
+BleService.tsx,7/1/718c74e323e71d471394a18adb9e403d788128a6
+8
+app.json,6/5/65596286f14dbaba6a5081a2edaae69700cc3c3b
+=
+tsconfig.json,6/1/61ebb9fd6e8cf9082658121d5d81e297791dacd0
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+O
+ios/cyberbadgemobile/Info.plist,6/d/6d377eec8434f9de01fae723222a4f9383906448
+H
+android/app/build.gradle,5/9/594ba479000e0bb5513707b709ad1cd52da9063a
+X
+(android/app/src/main/AndroidManifest.xml,e/f/ef79bfd51db69a15dbf5807c05bb249bdccb0556
---
+++ BleService.tsx
@@ -0,0 +1,217 @@
+import BleManager, {
+  BleConnectPeripheralEvent,
+  BleDisconnectPeripheralEvent,
+  BleManagerDidUpdateValueForCharacteristicEvent,
+  BleScanCallbackType,
+  BleScanMatchMode,
+  BleScanMode, Peripheral,
+  Peripheral as PeripheralWithoutConnectInfo,
+} from 'react-native-ble-manager';
+import {
+  EmitterSubscription,
+  NativeEventEmitter,
+  NativeModules,
+  PermissionsAndroid,
+  Platform,
+} from 'react-native';
+
+import Buffer from 'buffer';
+
+export type Peripheral = PeripheralWithoutConnectInfo & {
+  connected?: boolean;
+  connecting?: boolean;
+};
+
+interface EventsI {
+  bleManagerStartSuccess: () => void | undefined;
+  bleManagerStartError: () => void | undefined;
+  bleManagerStopScan: () => void | undefined;
+  bleManagerStartScan: () => void | undefined;
+  bleManagerDiscoverPeripheral: (peripheral: Peripheral) => void | undefined;
+  bleManagerDisconnectPeripheral: (
+    event: BleDisconnectPeripheralEvent,
+  ) => void | undefined;
+  bleManagerDidUpdateValueForCharacteristic: (
+    event: BleManagerDidUpdateValueForCharacteristicEvent,
+  ) => void | undefined;
+  bleManagerConnectPeripheral: (
+    event: BleConnectPeripheralEvent,
+  ) => void | undefined;
+};
+
+class Events implements EventsI {
+  bleManagerConnectPeripheral(event: BleConnectPeripheralEvent): void | undefined {
+    return undefined;
+  }
+
+  bleManagerDidUpdateValueForCharacteristic(event: BleManagerDidUpdateValueForCharacteristicEvent): void | undefined {
+    return undefined;
+  }
+
+  bleManagerDisconnectPeripheral(event: BleDisconnectPeripheralEvent): void | undefined {
+    return undefined;
+  }
+
+  bleManagerDiscoverPeripheral(peripheral: Peripheral): void | undefined {
+    return undefined;
+  }
+
+  bleManagerStartError(): void | undefined {
+    return undefined;
+  }
+
+  bleManagerStartScan(): void | undefined {
+    return undefined;
+  }
+
+  bleManagerStartSuccess(): void | undefined {
+    return undefined;
+  }
+
+  bleManagerStopScan(): void | undefined {
+    return undefined;
+  }
+}
+
+export default class BleService {
+  private _bleManagerModule = NativeModules.BleManager;
+  private _bleManagerEmitter = new NativeEventEmitter(this._bleManagerModule);
+  private _events: Events;
+  private _listeners: EmitterSubscription[];
+  private _peripherals: Map<Peripheral['id'], Peripheral>;
+
+  constructor() {
+    this._listeners = [];
+    this._events = new Events();
+    this._peripherals = new Map();
+    BleManager.start({showAlert: false})
+      .then(() => {
+        this.runEvent(this._events.bleManagerStartSuccess);
+        console.debug('[BleService]: BleManager started.');
+      })
+      .catch((err: any) => {
+        this.runEvent(this._events.bleManagerStartError);
+        console.debug('[BleService]: BeManager could not be started.', err);
+      });
+    this.handle_permissions();
+  }
+
+  setEvents(events: Events) {
+    this._events = events;
+    this.setupListiners(events);
+  }
+
+  handle_permissions() {
+    // po requestach, jak sie zrobi .then() nastepnie gdy (resoult) cos zwroci to oznacza, że ma perma jak nie to nie ma.
+    if (Platform.OS === 'android') {
+      if (Platform.Version >= 31)
+        PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+        ]);
+      else if (Platform.Version >= 23)
+        PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        );
+    }
+  }
+
+  scan() {
+    this.runEvent(this._events.bleManagerStartScan);
+    this.clearPeripherals();
+    BleManager.scan([], 3, false, {
+      matchMode: BleScanMatchMode.Sticky,
+      scanMode: BleScanMode.LowLatency,
+      callbackType: BleScanCallbackType.AllMatches,
+    })
+      .then(() => {
+        console.debug('[startScan] scan promise returned successfully.');
+      })
+      .catch((err: any) => {
+        console.error('[startScan] ble scan returned in error', err);
+      });
+  }
+
+  destroy() {
+    this.destroyListiners();
+  }
+
+  getPeripherals() {
+    return this._peripherals;
+  }
+
+  connect(p: Peripheral) {
+    return BleManager.connect(p.id);
+  }
+
+  async read(peripheral: Peripheral) {
+    // await BleManager.requestMTU(peripheral.id, 512);
+    const peripheralInfo = await BleManager.retrieveServices(peripheral.id);
+    console.log("Peripheral info:", peripheralInfo);
+    try {
+      const result = BleManager.startNotification(
+          peripheral.id,
+          '6e400001-b5a3-f393-e0a9-e50e24dcca9e',
+          '6e400003-b5a3-f393-e0a9-e50e24dcca9e',
+      );
+      console.log('Start notification result:', result);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  private addPeripheral(p: Peripheral) {
+    this._peripherals.set(p.id, p);
+  }
+
+  private clearPeripherals() {
+    this._peripherals = new Map();
+  }
+
+  private destroyListiners() {
+    for (const listener of this._listeners) {
+      if (listener) listener.remove();
+    }
+  }
+
+  private setupListiners(events: Events) {
+    this.destroyListiners();
+    this._listeners = [
+      this._bleManagerEmitter.addListener(
+        'BleManagerDiscoverPeripheral',
+        peripheral => {
+          this.addPeripheral(peripheral);
+          this.runEvent(events.bleManagerDiscoverPeripheral, peripheral);
+        },
+      ),
+      this._bleManagerEmitter.addListener('BleManagerStopScan', () =>
+        this.runEvent(events.bleManagerStopScan),
+      ),
+      this._bleManagerEmitter.addListener(
+        'BleManagerDisconnectPeripheral',
+        event => this.runEvent(events.bleManagerDisconnectPeripheral, event),
+      ),
+      this._bleManagerEmitter.addListener(
+        'BleManagerDidUpdateValueForCharacteristic',
+        event =>
+          this.runEvent(
+            events.bleManagerDidUpdateValueForCharacteristic,
+            event,
+          ),
+      ),
+      this._bleManagerEmitter.addListener(
+        'BleManagerConnectPeripheral',
+        event => this.runEvent(events.bleManagerConnectPeripheral, event),
+      ),
+    ];
+  }
+
+  private runEvent(
+    event: CallableFunction | undefined,
+    ...optionalParams: any[]
+  ): void {
+    if (event) {
+      event(...optionalParams);
+    }
+  }
+}
